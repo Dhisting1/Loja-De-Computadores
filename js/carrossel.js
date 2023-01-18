cd6006ea9fc2d1ca97388de4f69d85254aced6f8
Lucas Fernando Gonlçaves Lima <lucas.lima@estudante.ifb.edu.br>
2023-01-18 10:32:16 -0300

    f

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,4 +1,5 @@
 function Carrossel(config){
+    // Validação dos botões de prev e next, 
     this.container = ( typeof config.container === 'string') ? document.querySelector(config.container) : config.container
     
     this.itens = (typeof config.itens === 'string') ? this.container.querySelectorAll(config.itens) : config.itens
@@ -11,36 +12,38 @@ function Carrossel(config){
     var _currentSlide = 0;
     
     init()
+    // Função para adicionar a classe show nas figures
     function init(){
         var _show = _this.container.querySelectorAll('.show')
-        
+        // Array para percorrer a tag figure e adicionar e remover a classe show
         Array.prototype.forEach.call(_show, function(sh){
             sh.classList.remove('show')
         })
         _this.itens[0].classList.add('show')
         _this.btnNext.removeAttribute('style')
         _this.btnPrev.removeAttribute('style')
-        
+
+        // chamada de função para o evento 'click' nas setas
         addListeners()        
     }
-    
+    // adicionando o evento 'click' aos botões
     function addListeners(){
         _this.btnNext.addEventListener('click', showNextSlide)
         _this.btnPrev.addEventListener('click', showPrevSlide)
     }
-    
+    // função para incrementar +1 na variavel _current
     function showNextSlide(){
         _currentSlide++;
         showSlide()
     }
-    
+    // funcao para decrementar -1 a variavel _current
     function showPrevSlide(){
         _currentSlide--;
         showSlide()
     }
-    
+    // função que faz com que os botões do carrossel funcionem 
     function showSlide(){
-        var qtd = _this.itens.length;
+        var qtd = _this.itens.length; 
         var slide = _currentSlide % qtd;
         slide = Math.abs(slide);
         console.log(slide, qtd, _currentSlide)
